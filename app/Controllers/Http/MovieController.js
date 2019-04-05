70c33adfea126bc897476c4376610b26d7342417
adeadedoja <damdey@yahoo.com>
2019-04-05 18:59:14 +0200

    #165118780 - Get one movie by slug test passing

--- app/Controllers/Http/MovieController.js
+++ app/Controllers/Http/MovieController.js
@@ -1,16 +1,14 @@
-'use strict'
+'use strict';
 const Movie = use('App/Models/Movie');
 
 class MovieController {
     async all({ response, request }) {
         const movies = await Movie.all();
-
         return response.ok(movies);
     }
 
     async getMovieWithSlug({ response, params }) {
         const movie = await Movie.findByOrFail('movie_slug', params.slug);
-
         return response.ok(movie);
     }
 };
---
+++ test/functional/get-movie-with-slug.spec.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Factory = use('Factory');
+const { test, trait } = use('Test/Suite')('Get Movie With Slug');
+
+trait('Test/ApiClient');
+
+test('can get one movie with slug', async ({ assert, client }) => {
+    const numberOfMoviesToCreate = (Math.random() * (20 - 10 + 1)) << 0;
+
+    const movies = await Factory.model('App/Models/Movie').createMany(numberOfMoviesToCreate);
+
+    const randomNumber = Math.floor(Math.random() * (numberOfMoviesToCreate - 1)) + 1;
+    const randomMovie = movies[randomNumber];
+
+    const response = await client.get(`/api/movie/${randomMovie.movie_slug}`).end();
+
+    response.assertStatus(200);
+    response.assertJSONSubset(
+        { movie_name: randomMovie.movie_name, movie_genre: randomMovie.movie_genre }
+        );
+});
